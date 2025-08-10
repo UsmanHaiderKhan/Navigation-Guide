1f31b7f3e989222cefdc7e0a1849775e3b564b81
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-08-10 21:38:14 +0200

    ♻️ Update : Add product details route and links to Products page
    Introduced a new '/products/:productId' route in the router and updated the Products page to display a list of products with links to their respective detail pages.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Home from "./Pages/Home";
 import Products from "./Pages/Products";
 import RootLayout from "./Pages/Root";
 import ErrorPage from "./Pages/Error";
+import ProductDetails from "./Pages/Product-Details";
 
 const router = createBrowserRouter([
     {
         path: '/', element: <RootLayout/>,
         errorElement: <ErrorPage/>,
         children: [
+            // {index:true, element: <Home/>}, if parent route is activated
             {path: '/', element: <Home/>},
-            {path: '/products', element: <Products/>}
+            {path: '/products', element: <Products/>},
+            {path: '/products/:productId', element: <ProductDetails/>},
         ]
     },
 
