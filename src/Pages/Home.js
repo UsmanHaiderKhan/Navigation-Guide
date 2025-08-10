56002149d19d4ecb75c49ab541b73b28f2803f80
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-08-10 16:12:28 +0200

    ♻️ Update : add navigation button to Home page and update .gitignore
    Added a button to the Home page that navigates to the products page using useNavigate. Updated .gitignore to exclude the .idea folder.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,18 @@
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 function Home() {
+    const navigate = useNavigate();
+    function navigateHandler(){
+        navigate('/products');
+    }
     return(
         <>
             <h1>My Home Page</h1>
             <p>
                 Go to the
             <Link to="/products">List of Products</Link>
+            </p>
+            <p>
+                <button onClick={navigateHandler}>Products</button>
             </p>
         </>
 
